facd24a5e767140a65194f14a9937e4d9d065755
Gabriel Soft <iamgabrielsoft@gmail.com>
2021-04-20 20:44:25 +0100

    added the signin route

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+
+import SignupPage from './pages/signup'; 
+import SigninPage from './pages/signin'; 
 import ContactPage from './pages/contact';
 import AboutPage from './pages/about';
+import FeaturePage from './pages/Features'; 
 import FaqsPage from './pages/faqs';
+import PricingPage from './pages/pricing'; 
 import SupportPage from './pages/support';
 import IndexPage from './pages';
+
+
+
 
 class App extends Component {
 	render() {
@@ -13,12 +21,17 @@ class App extends Component {
 			<Router basename={process.env.PUBLIC_URL}>
 				
 				<Switch>
-					<Route exact path="/contact" component={ContactPage} />
-					<Route exact path="/about" component={AboutPage} />
-					<Route exact path="/faqs" component={FaqsPage} />
-					<Route exact path="/support" component={SupportPage} />
-					<Route exact path="/" component={IndexPage} />
+					<Route path="/pricing" component={PricingPage} />
+					<Route path="/feature" component={FeaturePage} />
+					<Route path="/signup" component={SignupPage} />
+					<Route path="/signin" component={SigninPage} />
+					<Route path="/contact" component={ContactPage} />
+					<Route path="/about" component={AboutPage} />
+					<Route path="/faqs" component={FaqsPage} />
+					<Route  path="/support" component={SupportPage} />
+					<Route path="/" component={IndexPage} />
 				</Switch>
+				
 			</Router>
 		);
 	}
